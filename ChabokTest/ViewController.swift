4d601c1411ff9e9704316fe9320a227d352f13e9
Parvin Mehrabani <Parvinmh74@gmail.com>
2017-10-31 15:21:01 +0330

    Initial Commit

---
+++ ChabokTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChabokTest
+//
+//  Created by Parvin Mehrabani on 8/9/1396 AP.
+//  Copyright © 1396 Parvin Mehrabani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
