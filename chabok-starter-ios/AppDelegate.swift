c88f7fbab28d201b229f14a7595fdeca436abc58
Hussein Habibi Juybari <Hussein.Juybari@gmail.com>
2019-05-18 23:48:20 +0430

    Send deep-link data

--- chabok-starter-ios/AppDelegate.swift
+++ chabok-starter-ios/AppDelegate.swift
@@ -111,6 +111,23 @@ class AppDelegate: UIResponder, UIApplicationDelegate , PushClientManagerDelegat
         }
     }
     
+    func application(_ app: UIApplication,
+                     open url: URL,
+                     options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        
+        manager?.appWillOpen(url)
+        return true
+    }
+    
+    func application(_ application: UIApplication,
+                     continue userActivity: NSUserActivity,
+                     restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
+        if (userActivity.activityType == NSUserActivityTypeBrowsingWeb) {
+            manager?.appWillOpen(userActivity.webpageURL)
+        }
+        return true
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
